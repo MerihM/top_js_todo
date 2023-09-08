6d479a56c1cf24bb9031df9d8ef8f4c910db11a1
MerihM <103122972+MerihM@users.noreply.github.com>
2023-09-08 23:29:55 +0200

    Import functions from task.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
-import * as test from './tasks/task.js';
+import * as task from './tasks/task.js';
 require('bootstrap-icons/font/bootstrap-icons.css');
 
 window.addEventListener('load', () => {
