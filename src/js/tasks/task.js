d6d86c6c65a942a0e054e32ec9fb36f2f39fbc5f
MerihM <103122972+MerihM@users.noreply.github.com>
2023-09-08 23:04:59 +0200

    Move editSave function

--- src/js/tasks/task.js
+++ src/js/tasks/task.js
@@ -41,3 +41,17 @@ function editModal(e, task) {
     let saveBtn = document.querySelector(`#save-edit-${findID}`);
     saveBtn.addEventListener('click', editSave);
 }
+
+function editSave(e, task) {
+    let findID = e.srcElement.id.replace('save-edit-', '');
+    let e_title = document.querySelector(`#edit-title-${findID}`);
+    let e_description = document.querySelector(`#edit-description-${findID}`);
+    let e_date = document.querySelector(`#edit-date-${findID}`);
+    let e_priority = document.querySelector(`#edit-priority-${findID}`);
+    let e_status = document.querySelector(`#edit-status-${findID}`);
+    task.title = e_title.value;
+    task.description = e_description.value;
+    task.dueDate = e_date.value;
+    task.priority = e_priority.value;
+    task.status = e_status.checked;
+}
