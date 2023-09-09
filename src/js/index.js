888bdc6825a62566f37542d29c6513845685e832
MerihM <103122972+MerihM@users.noreply.github.com>
2023-09-09 22:37:47 +0200

    Remove importing from pages.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,10 @@ import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 import * as task from './tasks/task.js';
 import * as project from './projects/project.js';
-import * as pages from './pages.js';
 require('bootstrap-icons/font/bootstrap-icons.css');
 
 window.addEventListener('load', () => {
-    pages.generateHome(1);
+    project.generateHome(1);
     // project.makeCardsP(project.sideArr);
     // task.makeCards(task.arrOfTasks);
 })
