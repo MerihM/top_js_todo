43e47cd529d55340da78572c1e9bf7c15c290b19
MerihM <103122972+MerihM@users.noreply.github.com>
2023-09-09 22:23:08 +0200

    Add generateHome function

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import '../scss/styles.scss';
 import * as bootstrap from 'bootstrap';
 import * as task from './tasks/task.js';
 import * as project from './projects/project.js';
+import * as pages from './pages.js';
 require('bootstrap-icons/font/bootstrap-icons.css');
 
 window.addEventListener('load', () => {
-    project.makeCardsP(project.sideArr);
-    task.makeCards(task.arrOfTasks);
+    pages.generateHome(1);
+    // project.makeCardsP(project.sideArr);
+    // task.makeCards(task.arrOfTasks);
 })
--- src/js/pages.js
+++ src/js/pages.js
@@ -1,3 +1,12 @@
 import * as bootstrap from 'bootstrap';
 import * as task from './tasks/task.js';
 import * as project from './projects/project.js';
+
+export function generateHome(index) {
+    let select = document.querySelector(`#project-container-${index}`);
+    project.makeCardsP(project.sideArr);
+    task.makeCards(project.sideArr[index].tasks);
+}
+export function generateUnfinished() {
+
+}
